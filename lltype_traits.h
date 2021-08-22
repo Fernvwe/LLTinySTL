837540840cfd009e717d239f56456bbef0a9f026
Fernvwe <780039590@qq.com>
2021-08-22 16:22:18 +0800

    add __type_traits specialization

--- lltype_traits.h
+++ lltype_traits.h
@@ -17,6 +17,100 @@ struct __type_traits{
     using has_trivial_destructor = __false_type;
     using is_POD_type = __false_type;
 };
+// specialization type_traits
+template<>
+struct __type_traits<char>{
+    using thsi_dummy_member_must_be_first = __true_type;
+    using has_trivial_default_constructor = __true_type;
+    using has_trivial_copy_constructor = __true_type;
+    using has_trivial_assignment_operator = __true_type;
+    using has_trivial_destructor = __true_type;
+    using is_POD_type = __true_type;
+};
+
+template<>
+struct __type_traits<unsigned char>{
+    using thsi_dummy_member_must_be_first = __true_type;
+    using has_trivial_default_constructor = __true_type;
+    using has_trivial_copy_constructor = __true_type;
+    using has_trivial_assignment_operator = __true_type;
+    using has_trivial_destructor = __true_type;
+    using is_POD_type = __true_type;
+};
+template<>
+struct __type_traits<short>{
+    using thsi_dummy_member_must_be_first = __true_type;
+    using has_trivial_default_constructor = __true_type;
+    using has_trivial_copy_constructor = __true_type;
+    using has_trivial_assignment_operator = __true_type;
+    using has_trivial_destructor = __true_type;
+    using is_POD_type = __true_type;
+};
+template<>
+struct __type_traits<unsigned short>{
+    using thsi_dummy_member_must_be_first = __true_type;
+    using has_trivial_default_constructor = __true_type;
+    using has_trivial_copy_constructor = __true_type;
+    using has_trivial_assignment_operator = __true_type;
+    using has_trivial_destructor = __true_type;
+    using is_POD_type = __true_type;
+};template<>
+struct __type_traits<int>{
+    using thsi_dummy_member_must_be_first = __true_type;
+    using has_trivial_default_constructor = __true_type;
+    using has_trivial_copy_constructor = __true_type;
+    using has_trivial_assignment_operator = __true_type;
+    using has_trivial_destructor = __true_type;
+    using is_POD_type = __true_type;
+};
+template<>
+struct __type_traits<unsigned int>{
+    using thsi_dummy_member_must_be_first = __true_type;
+    using has_trivial_default_constructor = __true_type;
+    using has_trivial_copy_constructor = __true_type;
+    using has_trivial_assignment_operator = __true_type;
+    using has_trivial_destructor = __true_type;
+    using is_POD_type = __true_type;
+};
+template<>
+struct __type_traits<long>{
+    using thsi_dummy_member_must_be_first = __true_type;
+    using has_trivial_default_constructor = __true_type;
+    using has_trivial_copy_constructor = __true_type;
+    using has_trivial_assignment_operator = __true_type;
+    using has_trivial_destructor = __true_type;
+    using is_POD_type = __true_type;
+};
+
+template<>
+struct __type_traits<unsigned long>{
+    using thsi_dummy_member_must_be_first = __true_type;
+    using has_trivial_default_constructor = __true_type;
+    using has_trivial_copy_constructor = __true_type;
+    using has_trivial_assignment_operator = __true_type;
+    using has_trivial_destructor = __true_type;
+    using is_POD_type = __true_type;
+};
+
+template<>
+struct __type_traits<double>{
+    using thsi_dummy_member_must_be_first = __true_type;
+    using has_trivial_default_constructor = __true_type;
+    using has_trivial_copy_constructor = __true_type;
+    using has_trivial_assignment_operator = __true_type;
+    using has_trivial_destructor = __true_type;
+    using is_POD_type = __true_type;
+};
+template <class T>
+struct __type_traits<T*>{
+    using thsi_dummy_member_must_be_first = __true_type;
+    using has_trivial_default_constructor = __true_type;
+    using has_trivial_copy_constructor = __true_type;
+    using has_trivial_assignment_operator = __true_type;
+    using has_trivial_destructor = __true_type;
+    using is_POD_type = __true_type;
+};
+
 }
 
 #endif
